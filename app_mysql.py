a0f4f55b83c402ced0a92d8efff2cc32d79b3f1c
SENAC\eduardo33099086 <eduardo33099086@edu.df.senac.br>
2025-12-05 21:43:51 -0300

    .

--- app_mysql.py
+++ app_mysql.py
@@ -216,7 +216,7 @@ def perfil():
     user_id = session["user_id"]
     try:
         user_model = Usuario.get_by_id(user_id)
-        usuario = model_to_dict(user_model, fields=[Usuario.nome, Usuario.email])
+        usuario = model_to_dict(user_model, fields_from_query=[Usuario.nome, Usuario.email])
     except DoesNotExist:
         usuario = None
 
